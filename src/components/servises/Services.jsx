a3799d208a61d85df7a7c984439a0f4935d18af4
Arslan Baratov <baratovarslan24@gmail.com>
2026-01-16 15:50:37 +0600

    add services

---
+++ src/components/servises/Services.jsx
@@ -0,0 +1,11 @@
+import ServicesTitle from "./ServicesTitle";
+import ServicesCard from "./ServicesCard";
+
+export default function Services() {
+  return (
+    <div>
+      <ServicesTitle />
+      <ServicesCard />
+    </div>
+  );
+}
---
+++ src/components/servises/ServicesTitle.jsx
@@ -0,0 +1,9 @@
+export default function ServicesTitle() {
+  return (
+    <div>
+      <h2 className="font-bold text-[28px] text-[#333] text-center mb-7.5 md:text-5xl md:text-start md:mb-15">
+        Послуги
+      </h2>
+    </div>
+  );
+}
