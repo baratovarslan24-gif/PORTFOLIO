8507b3db0c6c521c3d9126fcb65da39e1cc36adb
Arslan Baratov <baratovarslan24@gmail.com>
2026-01-19 15:27:00 +0600

    fixed the skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,12 +1,13 @@
-import SkillsTitle from "./SkillsTitle";
+import SectionTitle from "../section-title/SectionTitle.jsx";
 import SkillsImage from "./SkillsImage";
 import SkillsDiagrams from "./SkillsDiagrams";
 import SkillImage from "../../assets/images/skills-img.jpg";
+import { skillsContent } from "../../data/app.js";
 
 export default function Skills() {
   return (
     <div>
-      <SkillsTitle />
+      <SectionTitle text={skillsContent.title} />
       <div className="px-5.5 md:flex md:gap-22.5 md:px-0">
         <SkillsImage image={SkillImage} altText="Skills Image" />
         <SkillsDiagrams />
--- src/components/skills/SkillsDiagrams.jsx
+++ src/components/skills/SkillsDiagrams.jsx
@@ -1,34 +1,36 @@
 import "./skills.css";
+
+import { skillsContent } from "../../data/app.js";
 export default function SkillsDiagrams() {
   return (
     <div className="md:mt-20">
       <ul>
         <li className="md:flex">
           <p className="font-semibold text-lg text-center text-[#333] mb-1.25 uppercase md:mr-2.5 md:text-start">
-            html
+            {skillsContent.diagram[0].skills}
           </p>
           <progress
-            value="510"
+            value={skillsContent.diagram[0].level}
             max="600"
             className="w-75 mb-6 md:w-150 h-8  md:mb-21.5"
           ></progress>
         </li>
         <li className="md:flex">
           <p className="font-semibold text-lg text-center text-[#333] mb-1.25 uppercase md:mr-6">
-            css
+            {skillsContent.diagram[1].skills}
           </p>
           <progress
-            value="480"
+            value={skillsContent.diagram[1].level}
             max="600"
             className="w-75 mb-6 md:w-150 h-8  md:mb-21.5"
           ></progress>
         </li>
         <li className="md:flex">
           <p className="font-semibold text-lg text-center text-[#333] mb-1.25 uppercase md:mr-9">
-            js
+            {skillsContent.diagram[2].skills}
           </p>
           <progress
-            value="366"
+            value={skillsContent.diagram[2].level}
             max="600"
             className="w-75 mb-12.5 md:w-150 h-8  "
           ></progress>
