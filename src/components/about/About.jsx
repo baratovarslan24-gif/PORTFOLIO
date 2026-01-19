a31ad6ee687a623b8bd17aa769a60507a2e2936a
Arslan Baratov <baratovarslan24@gmail.com>
2026-01-19 15:17:16 +0600

    fixed the about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,11 @@
-import AboutTitle from "./AboutTitle";
+import SectionTitle from "../section-title/SectionTitle.jsx";
 import AboutText from "./AboutText.jsx";
+import { aboutContent } from "../../data/app.js";
 
 export default function About() {
   return (
     <div className="mt-12.5 md:mt-35 mb-12.5 md:mb-35">
-      <AboutTitle />
+      <SectionTitle text={aboutContent.title} />
       <AboutText />
     </div>
   );
---
+++ src/components/section-title/SectionTitle.jsx
@@ -0,0 +1,7 @@
+export default function SectionTitle({ text }) {
+  return (
+    <h2 className="font-bold text-[28px] text-[#333333] text-center mb-7.5 md:text-start md:text-5xl md:mb-15">
+      {text}
+    </h2>
+  );
+}
